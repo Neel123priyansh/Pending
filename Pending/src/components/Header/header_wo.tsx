99d78220f4010ed83d31c9e33a4f2131fd0d9d80
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-13 22:07:01 +0530

    UI/UX Finishing

--- Pending/src/components/Header/header_wo.tsx
+++ Pending/src/components/Header/header_wo.tsx
@@ -8,7 +8,7 @@ export default function headerwo() {
 
   return (
     <div className='fixed z-50 w-full backdrop-blur-lg bg-white/5 flex justify-between items-center max-w-[100%] text-white h-24 bg-[#f7efd8] mx-auto'>
-        <h1 onClick={() => navigate('/')} className='text-5xl font-bold text-[#00df9a] ml-10'>Pending.</h1>
+        <h1 onClick={() => navigate('/')} className='text-5xl font-bold text-[#00df9a] ml-10'>Pending ha.</h1>
         <ul className='flex flex-row text-lg text-black  gap-8 mr-[460px]'>
         </ul>
     </div>
