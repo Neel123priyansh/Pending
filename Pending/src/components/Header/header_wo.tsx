08570e30c3ec515f8decfa9a19f5bbc5596e27f4
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-13 22:07:28 +0530

    UI/UX Finishing

--- Pending/src/components/Header/header_wo.tsx
+++ Pending/src/components/Header/header_wo.tsx
@@ -1,14 +1,13 @@
 import { useRef } from "react";
 import { useNavigate} from "react-router-dom" 
 import about from "../About/about";
-
 export default function headerwo() {
   const navigate = useNavigate()
   const aboutus = useRef(null);
 
   return (
     <div className='fixed z-50 w-full backdrop-blur-lg bg-white/5 flex justify-between items-center max-w-[100%] text-white h-24 bg-[#f7efd8] mx-auto'>
-        <h1 onClick={() => navigate('/')} className='text-5xl font-bold text-[#00df9a] ml-10'>Pending nhi ha.</h1>
+        <h1 onClick={() => navigate('/')} className='text-5xl font-bold text-[#00df9a] ml-10'>Pending.</h1>
         <ul className='flex flex-row text-lg text-black  gap-8 mr-[460px]'>
         </ul>
     </div>
