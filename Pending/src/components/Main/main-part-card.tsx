ea702329123eb459269c58d2b00ee3aef9578750
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-15 23:49:03 +0530

    UI/UX Finishing

--- Pending/src/components/Main/main-part-card.tsx
+++ Pending/src/components/Main/main-part-card.tsx
@@ -27,7 +27,7 @@ function main_part_card() {
         <Vortex particleCount={100} rangeY={800}>
        <button onClick={() => navigate('Info-Page') }>
         <Tilt options={defaultOptions}>
-            <div className='h-[320px] rounded-2xl w-[240px] font-inter ml-20 text-2xl text-[#303640] bg-white'>
+            <div className='h-[320px] rounded-2xl w-[240px] font-inter ml-20 text-3xl text-[] bg-[#e4dddd]'>
             <img className='rounded-t-2xl ' src={Ass}/>
             <p className='pr-16 pt-2'>Assingment</p> 
         </div>
