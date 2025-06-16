06e755c16d073d846276cc718dab71fa64b1c3d8
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-16 18:30:47 +0530

    UI/UX Finishing

--- Pending/src/components/About/about.tsx
+++ Pending/src/components/About/about.tsx
@@ -1,17 +1,19 @@
 import { forwardRef } from "react";
 import { useRef } from "react";
+import Aboutna from '../../../src/assets/Aboutna.png'
 
 function about() {
   return (
     <div id="aboutus" className='mt-20 flex flex-col items-center'>
         <div className='flex flex-col font-urbanist '>
-          <p className="-tracking-tighter text-3xl text-[#301934]">WORK YOU CAN TRUST</p>
+          <p className="-tracking-tighter text-center text-3xl text-[#301934]">WORK YOU CAN TRUST</p>
           <div className="flex flex-row my-5 text-7xl justify-center text-[#301934]">
           <p className='mr-3'>About</p><p className='text-[#00df9a]'>Pen</p><p>ding</p>
           </div>
         </div>
-        <div className=' text-white mx-10 h-max max-w-[100%] bg-[#301934] font-urbanist rounded-3xl p-10'>
-        <p className='text-2xl'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
+        <div className=' text-white flex flex-row mx-10 h-max max-w-[100%] bg-[#301934] font-urbanist rounded-3xl p-10'>
+        <p className='text-2xl pt-4'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
+        <img src={Aboutna} className='size-96'/>
         </div>
         
     </div>
--- Pending/src/components/Main/main-part.tsx
+++ Pending/src/components/Main/main-part.tsx
@@ -1,13 +1,14 @@
 import { ReactTyped } from 'react-typed'
+import { WordRotate } from "@/components/magicui/word-rotate";
 
 export default function main() {
   return (
     <div className='flex flex-col justify-center'>
-        <div className='max-w-[100%] text-center mt-[120px] w-full text-[#301934] font-playfair h-10 flex justify-center items-center md:text-6xl sm:text-6xl'> 
+        <div className='max-w-[100%] text-center mt-[120px] w-full text-[#301934] font-urbanist h-10 flex justify-center items-center md:text-6xl sm:text-6xl'> 
          Turning Tasks into Triumphs!  
         </div>
-        <div className='max-w-[100%]  text-center mt-[30px] text-[#301934] mb-5 rounded-b-xl w-full h-10 flex flex-row  justify-center items-center font-playfair font-[900] md:text-6xl sm:text-6xl'> 
-        <p className='mr-3'>Your,</p><ReactTyped strings={[ 'Assingment', 'Lab File', 'Mini Projects']} typeSpeed={60} backSpeed={70} loop/>  
+        <div className='max-w-[100%]  text-center mt-[30px] text-[#301934] mb-5 rounded-b-xl w-full h-10 flex flex-row  justify-center items-center font-urbanist md:text-6xl sm:text-6xl'> 
+        <p className='mr-3'>Your,</p><WordRotate words={[ 'Assingment', 'Lab File', 'Mini Projects']}/>  
         </div>
     </div>
   )
