73d6dd9fc1c762caea8d79e8d9e0c96e8ac94808
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-17 00:05:50 +0530

    UI/UX Finishing

--- Pending/src/components/About/about.tsx
+++ Pending/src/components/About/about.tsx
@@ -4,23 +4,15 @@ import Aboutna from '../../../src/assets/Aboutna.png'
 
 function about() {
   return (
-    <div id="aboutus" className='mt-20 flex flex-col'>
-        <div className='flex flex-col font-urbanist '>
-          <p className="-tracking-tighter text-center text-3xl text-[#301934]">WORK YOU CAN TRUST</p>
-          <div className="flex flex-row my-5 text-7xl justify-center text-[#301934] ">
-          <p className='mr-3'>About</p><p className='text-[#00df9a]'>Pen</p><p>ding</p>
-          </div>
-        </div>
-        <div className="flex flex-row">
+    <div id="aboutus" className='my-20 flex gap-0 flex-row'>
         <div className=" flex flex-col">
-        <div className=' text-white flex flex-row ml-10 h-max max-w-[70%] bg-[#301934] font-urbanist rounded-tl-3xl p-10'>
+        <div className=' text-white ml-10 w-[100%] bg-[#301934] font-urbanist rounded-tl-3xl p-10'>
         <p className='text-2xl pt-2'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
         </div>
-        <div className=' text-white flex flex-row ml-10 h-max max-w-[70%] bg-[#301934] font-urbanist rounded-bl-3xl mt-1 p-10'/>
+        <div className='ml-10 h-max w-[96%] bg-[#301934] rounded-bl-3xl mt-1 p-10'/>
         </div>
-        <div className=' text-white h-max max-w-[100%] bg-[#301934] font-urbanist rounded-tr-3xl rounded-br-3xl'>
-        <img src={Aboutna}/>
-        </div>
+        <div className= 'bg-[#301934] w-[150%] rounded-tr-3xl mr-10 rounded-br-3xl'>
+        <img className="mt-5" src={Aboutna}/>
         </div>
     </div>
   )
---
+++ Pending/src/components/About/text.tsx
@@ -0,0 +1,6 @@
+        <div className='flex flex-col font-urbanist '>
+          <p className="-tracking-tighter text-center text-3xl text-[#301934]">WORK YOU CAN TRUST</p>
+          <div className="flex flex-row my-5 text-7xl justify-center text-[#301934] ">
+          <p className='mr-3'>About</p><p className='text-[#00df9a]'>Pen</p><p>ding</p>
+          </div>
+        </div>
