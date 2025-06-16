73cc94c4b736572b2aa3c2a49f036145b4f8604d
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-16 22:02:42 +0530

    UI/UX Finishing

--- Pending/src/components/About/about.tsx
+++ Pending/src/components/About/about.tsx
@@ -7,15 +7,21 @@ function about() {
     <div id="aboutus" className='mt-20 flex flex-col'>
         <div className='flex flex-col font-urbanist '>
           <p className="-tracking-tighter text-center text-3xl text-[#301934]">WORK YOU CAN TRUST</p>
-          <div className="flex flex-row my-5 text-7xl justify-center text-[#301934]">
+          <div className="flex flex-row my-5 text-7xl justify-center text-[#301934] ">
           <p className='mr-3'>About</p><p className='text-[#00df9a]'>Pen</p><p>ding</p>
           </div>
         </div>
-        <div className=' text-white flex flex-row mx-10 h-max max-w-[100%] bg-[#301934] font-urbanist rounded-3xl p-10'>
-        <p className='text-2xl pt-4'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
-        <img src={Aboutna} className='size-96'/>
+        <div className="flex flex-row">
+        <div className=" flex flex-col">
+        <div className=' text-white flex flex-row ml-10 h-max max-w-[70%] bg-[#301934] font-urbanist rounded-tl-3xl p-10'>
+        <p className='text-2xl pt-2'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
         </div>
-        <div className="h-10 w-10 m-10"></div>
+        <div className=' text-white flex flex-row ml-10 h-max max-w-[70%] bg-[#301934] font-urbanist rounded-bl-3xl mt-1 p-10'/>
+        </div>
+        <div className=' text-white h-max max-w-[100%] bg-[#301934] font-urbanist rounded-tr-3xl rounded-br-3xl'>
+        <img src={Aboutna}/>
+        </div>
+        </div>
     </div>
   )
 }
