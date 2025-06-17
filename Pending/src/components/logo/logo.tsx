aa4e52b71a60112882254babe6383ecaccb2a397
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-17 23:57:29 +0530

    UI/UX Finishing

--- Pending/src/components/logo/logo.tsx
+++ Pending/src/components/logo/logo.tsx
@@ -2,11 +2,13 @@ import srm from '../../assets/SRMM-removebg-preview.png'
 import srmsoni from '../../assets/SRMSoni-removebg-preview.png'
 import srmncr from '../../assets/SRMNCR-removebg-preview.png'
 import srmap from '../../assets/srmap-removebg-preview.png'
+import Marquee from 'react-fast-marquee';
+
 function logo() {
   return (
     <div className="w-100% gap-20 my-40 h-max overflow-x-hidden items-center px-40  flex flex-col">
       <p className='font-urbanist text-3xl text-[#301934] -tracking-tighter'>TRUSTED BY STUDENTS AT</p>
-      <div className='flex flex-row gap-60 animate-marquee whitespace-nowrap'>
+      <Marquee>
         <img src={srm} className='h-20'/>
         <img src={srmncr} className='h-20'/>
         <img src={srmsoni} className='h-20'/>
