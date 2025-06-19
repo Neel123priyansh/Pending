9251e9d8fe7e4f8a19b75a2af800b12e9a0eb66a
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-19 22:07:09 +0530

    UI/UX Fixing

--- Pending/src/components/About/about.tsx
+++ Pending/src/components/About/about.tsx
@@ -8,7 +8,7 @@ import people from "./people";
 
 function about() {
   return (
-    <div id="aboutus" className='my-10 flex flex-row'>
+    <div id="aboutus" className=' flex flex-row'>
         <div className=" flex flex-col">
         <div className=' text-white ml-10 w-[96%] bg-[#301934] font-urbanist rounded-tl-3xl py-10 px-4'>
         <p className='text-2xl pt-2'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -1,7 +1,7 @@
 function people() {
   return (
     <div className=" flex flex-col">
-        <div className=' text-white ml-10 w-[96%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
+        <div className=' text-white ml-10 mt-2 w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
         <p className='text-2xl pt-2'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
         </div>
     </div>
