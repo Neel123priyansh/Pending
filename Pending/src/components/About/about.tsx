801bfc630808b9b91a0c13e78a387a046308401a
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-19 21:55:11 +0530

    UI/UX Fixing

--- Pending/src/components/About/about.tsx
+++ Pending/src/components/About/about.tsx
@@ -23,9 +23,6 @@ function about() {
       />
       </div>
         </div>
-        <div className=' text-white mt-10 ml-10 w-[96%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
-        <p className='text-2xl pt-2'>Pending is an online platform connecting ambitious students and top experts from all over the world. It's a great place where people can cooperate with skilled professionals in any subject to succeed in learning or share their expertise with those who seek help. You can find an expert in any subject here on Studybay and connect with them directly via our secure chat, without intermediaries. At Pending, we are dedicated to helping you succeed in your academic endeavors. Our platform is built on the principles of trust, expertise, and seamless communication, making it the ideal place to find the support you need to achieve your goals. Explore our platform today and take the next step in your educational journey with Pending.</p>
-        </div>
         </div>
         <div className= 'w-[150%] items-center overflow-hidden rounded-tr-3xl mr-10'>
         <img className="w-full h-full object-cover" src={Climb}/>
