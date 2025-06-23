642a114d5b4f32170e60c5aa2aff82053ebeb6e6
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-23 23:59:08 +0530

    UI/UX Final

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -2,12 +2,15 @@ import { BackgroundGradient } from "../ui/background-gradient";
 
 function people() {
   return (
-    <div className=" flex flex-row">
-        <div className=' flex flex-row ml-10 mt-[6px] justify-evenly w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
+    <div className=" flex flex-col ml-10 mt-[6px] justify-evenly w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4">
+        <div className=' flex flex-row justify-evenly'>
         <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
+        </div>
+        <div className="flex flex-row my-1 mt-3 text-7xl justify-center ">
+          <p className='text-[#00df9a]'>Made</p>
         </div>
     </div>
   )
