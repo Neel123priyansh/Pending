dade35a6d4439fa5b18605d4f2945a5dad40e33c
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-23 23:36:40 +0530

    UI/UX Final

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -1,8 +1,11 @@
+import { BackgroundGradient } from "../ui/background-gradient";
+
 function people() {
   return (
     <div className=" flex flex-row">
-        <div className=' text-white ml-10 mt-[6px] w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
-        
+        <div className=' flex flex-row ml-10 mt-[6px]  w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900"></BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900"></BackgroundGradient>
         </div>
     </div>
   )
