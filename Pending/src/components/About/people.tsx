6683db518d6251da2adcb6aaa99e4e1e7fae4924
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-23 23:52:52 +0530

    UI/UX Final

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -4,10 +4,10 @@ function people() {
   return (
     <div className=" flex flex-row">
         <div className=' flex flex-row ml-10 mt-[6px] justify-evenly w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
-        <div className="w-[19rem] h-[25rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
-        <div className="w-[19rem] h-[25rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
-        <div className="w-[19rem] h-[25rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
-        <div className="w-[19rem] h-[25rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]"></div>
         </div>
     </div>
   )
