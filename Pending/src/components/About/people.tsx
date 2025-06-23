d9cdd753966fba9c5c17571ac83f34c5e52cc8d9
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-23 23:47:45 +0530

    UI/UX Final

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -4,10 +4,10 @@ function people() {
   return (
     <div className=" flex flex-row">
         <div className=' flex flex-row ml-10 mt-[6px] justify-evenly w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4'>
-        <BackgroundGradient className="  p-4 sm:p-10 bg-white"></BackgroundGradient>
+        <BackgroundGradient className="  p-4 h-60 w-60 rounded-md bg-white"></BackgroundGradient>
+        {/* <BackgroundGradient className="  p-4 sm:p-10 bg-white "></BackgroundGradient>
         <BackgroundGradient className="  p-4 sm:p-10 bg-white "></BackgroundGradient>
-        <BackgroundGradient className="  p-4 sm:p-10 bg-white "></BackgroundGradient>
-        <BackgroundGradient className="  p-4 sm:p-10 bg-white "></BackgroundGradient>
+        <BackgroundGradient className="  p-4 sm:p-10 bg-white "></BackgroundGradient> */}
         </div>
     </div>
   )
