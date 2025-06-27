9e2cc77a21cb2f1f93ab9e0a8d4d5a3c2b2faf3c
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-27 15:11:05 +0530

    UI/UX

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -5,9 +5,9 @@ function people() {
   return (
     <div className=" flex flex-col ml-10 mt-[6px] justify-evenly w-[94.8%] bg-[#301934] font-urbanist rounded-b-3xl py-10 px-4">
         <div className=' flex flex-row justify-evenly'>
-        <div className="w-[19rem] h-[22rem] rounded-[30px] items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
+        <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
-          <h1>SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
+          <h1 className="text-xl text-center">SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
         </div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
