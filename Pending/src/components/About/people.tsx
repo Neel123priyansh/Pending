9691c2a8856b78028959be17b1d819daf88747b3
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-27 15:17:18 +0530

    UI/UX

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -7,19 +7,19 @@ function people() {
         <div className=' flex flex-row justify-evenly'>
         <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
+          <h1 className="text-xl font-urbanist text-center">SRM IST Delhi-NCR Campus, Ghaziabad</h1>
+        </div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
+          <img className="h-56 w-56" src={perimg}/>
+          <h1 className="text-xl font-urbanist text-center">SRM University, Kattankulathur</h1>
+        </div>
+        <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
+          <img className="h-56 w-56" src={perimg}/>
           <h1 className="text-xl font-urbanist text-center">SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
         </div>
-        <div className="w-[19rem] h-[22rem] rounded-[30px] items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
+        <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
-          <h1>SRM-IST</h1>
-        </div>
-        <div className="w-[19rem] h-[22rem] rounded-[30px] items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
-          <img className="h-56 w-56" src={perimg}/>
-          <h1>SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
-        </div>
-        <div className="w-[19rem] h-[22rem] rounded-[30px] items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
-          <img className="h-56 w-56" src={perimg}/>
-          <h1>SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
+          <h1 className="text-xl font-urbanist text-center">SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
         </div>
         </div>
         <div className="flex flex-row my-1 mt-3 text-7xl justify-center ">
