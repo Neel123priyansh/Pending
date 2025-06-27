0d4f5b45b4704d52aea67226bbfecc3c633189ad
Priyansh Neel <neelpriyansh@gmail.com>
2025-06-27 15:03:33 +0530

    UI/UX

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -18,7 +18,8 @@ function people() {
           <h1>SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
         </div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
-          <img src={perimg}/>
+          <img className="h-56 w-56" src={perimg}/>
+          <h1>SRM-IST Delhi-NCR Campus, Ghaziabad</h1>
         </div>
         </div>
         <div className="flex flex-row my-1 mt-3 text-7xl justify-center ">
