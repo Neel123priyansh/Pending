944ca2cec3b5d4e2bd594f0fb22a917e3c94728f
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-03 23:38:23 +0530

    Backend Connect

--- Pending/src/components/About/people.tsx
+++ Pending/src/components/About/people.tsx
@@ -11,7 +11,7 @@ function people() {
         </div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
-          <h1 className="text-xl font-urbanist text-center">SRM Uniersity, Kattankulathur</h1>
+          <h1 className="text-xl font-urbanist text-center">SRM University, Kattankulathur</h1>
         </div>
         <div className="w-[19rem] h-[22rem] rounded-[30px] gap-10 items-center flex flex-col justify-center bg-[#ece5e5] shadow-[inset_0_30px_50px_-12px_rgba(50,50,93,0.25),inset_0_18px_26px_-18px_rgba(0,0,0,0.3)]">
           <img className="h-56 w-56" src={perimg}/>
